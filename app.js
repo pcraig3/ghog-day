1ae91034ea9a384f014e7694f6b668512995c9f6
Paul Craig <paul@pcraig3.ca>
2022-03-12 21:50:48 -0500

    Rename "users" to "predictions"

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ nunjucks.configure('views', {
 })
 
 var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
+var usersRouter = require('./routes/predictions')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -41,7 +41,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/predictions', usersRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
