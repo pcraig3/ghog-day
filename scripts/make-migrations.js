d64441701af29b8bc7fee86d5213a2189e5c3aaa
Paul Craig <paul@pcraig3.ca>
2022-03-13 04:58:05 -0400

    Get the migrations file sequenced properly

--- scripts/make-migrations.js
+++ scripts/make-migrations.js
@@ -1,7 +1,6 @@
 const fs = require('fs')
 const path = require('path')
 const csv = require('fast-csv')
-const stream = fs.createWriteStream('./migrations/001-init.sql')
 const { escape, hasShadow } = require('./utils')
 
 const migrationsStart = `-- Up
@@ -20,10 +19,9 @@ CREATE TABLE predictions (
   year INTEGER,
   shadow BOOLEAN,
   details TEXT,
+  FOREIGN KEY(ghogId) REFERENCES groundhogs(id),
   UNIQUE (ghogId, year)
 );
-
-/* Insert rows */
 `
 
 const migrationsEnd = `
@@ -32,47 +30,95 @@ DROP TABLE IF EXISTS predictions;
 DROP TABLE IF EXISTS groundhogs;
 `
 
-stream.write(migrationsStart)
+const insertMigrationsStart = () => {
+  return new Promise(function (resolve, reject) {
+    stream = fs.createWriteStream('./migrations/001-init.sql')
+    stream.write(migrationsStart)
+
+    // end stream
+    stream.end()
+    console.log('start')
+    resolve()
+  })
+}
 
 /* Insert groundhogs */
-fs.createReadStream(path.resolve(__dirname, '../csv', 'groundhogs.csv'))
-  .pipe(csv.parse({ headers: true }))
-  .on('error', (error) => console.error(error))
-  .on('data', (row) => {
-    const insert = `INSERT INTO groundhogs (id, name, fullname, city, country, source) VALUES (${parseInt(
-      row.id,
-    )}, '${escape(row.name)}', '${escape(row.fullname)}', '${escape(row.city)}', '${escape(
-      row.country,
-    )}', '${escape(row.source)}');\n`
 
-    stream.write(insert)
+const insertGroundhogs = () => {
+  return new Promise(function (resolve, reject) {
+    stream = fs.createWriteStream('./migrations/001-init.sql', { flags: 'a' })
+    stream.write('\n/* Insert Groundhogs */\n')
+
+    fs.createReadStream(path.resolve(__dirname, '../csv', 'groundhogs.csv'))
+      .pipe(csv.parse({ headers: true }))
+      .on('error', (error) => {
+        console.error(error)
+        reject(error)
+      })
+      .on('data', (row) => {
+        const insert = `INSERT INTO groundhogs (id, name, fullname, city, country, source) VALUES (${parseInt(
+          row.id,
+        )}, '${escape(row.name)}', '${escape(row.fullname)}', '${escape(row.city)}', '${escape(
+          row.country,
+        )}', '${escape(row.source)}');\n`
+
+        stream.write(insert)
+      })
+      .on('end', (rowCount) => {
+        console.log(`Inserted ${rowCount} groundhogs`)
+        stream.end()
+        resolve()
+      })
   })
-  .on('end', (rowCount) => {
+}
+
+const insertPredictions = () => {
+  return new Promise(function (resolve, reject) {
+    const filenames = fs.readdirSync(path.resolve(__dirname, '../csv'))
+    const predictions = filenames.filter((filename) => filename.startsWith('prediction'))
+
+    stream = fs.createWriteStream('./migrations/001-init.sql', { flags: 'a' })
+    stream.write('\n/* Insert Predictions */\n')
+
+    /* Insert predictions */
+    predictions.map((file) => {
+      const [suffix, id, name] = file.split('_')
+      fs.createReadStream(path.resolve(__dirname, '../csv', file))
+        .pipe(csv.parse({ headers: true }))
+        .on('error', (error) => console.error(error))
+        .on('data', (row) => {
+          const insert = `INSERT INTO predictions (ghogId, year, shadow, details) VALUES (${parseInt(
+            id,
+          )}, ${parseInt(row.year)}, ${hasShadow(row.shadow)}, '${escape(row.details)}');\n`
+
+          stream.write(insert)
+        })
+        .on('end', (rowCount) => {
+          console.log(`Parsed ${rowCount} rows`)
+
+          if (file === predictions[predictions.length - 1]) {
+            stream.end()
+            resolve()
+          }
+        })
+    })
+    console.log(predictions)
+  })
+}
+
+const insertMigrationsEnd = () => {
+  return new Promise(function (resolve, reject) {
+    stream = fs.createWriteStream('./migrations/001-init.sql', { flags: 'a' })
     stream.write(migrationsEnd)
 
     // end stream
     stream.end()
-
-    console.log(`Inserted ${rowCount} groundhogs`)
+    console.log('done')
+    resolve()
   })
+}
 
-/* Insert predictions */
-
-// fs.createReadStream(path.resolve(__dirname, '../csv', '2_willie.csv'))
-//   .pipe(csv.parse({ headers: true }))
-//   .on('error', (error) => console.error(error))
-//   .on('data', (row) => {
-//     const insert = `INSERT INTO predictions (ghogId, year, shadow, details) VALUES (2, ${parseInt(
-//       row.year,
-//     )}, ${hasShadow(row.shadow)}, '${escape(row.details)}');\n`
-
-//     stream.write(insert)
-//   })
-//   .on('end', (rowCount) => {
-//     stream.write(migrationsEnd)
-
-//     // end stream
-//     stream.end()
-
-//     console.log(`Parsed ${rowCount} rows`)
-//   })
+insertMigrationsStart()
+  .then(() => insertGroundhogs())
+  .then(() => insertPredictions())
+  .then(() => insertMigrationsEnd())
