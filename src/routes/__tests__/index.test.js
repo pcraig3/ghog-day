5e236223c3f8c197c12fd3eb19c443b0ca8f57bb
Paul Craig <paul@pcraig3.ca>
2026-02-02 14:51:41 -0500

    Add new groundhog: Portage la Prairie Larry

--- src/routes/__tests__/index.test.js
+++ src/routes/__tests__/index.test.js
@@ -242,11 +242,11 @@ describe('Test API responses', () => {
   })
 
   describe('for /api/v1/groundhogs path', () => {
-    const GROUNDHOGS_ALL = 88
-    const GROUNDHOGS_CANADA = 14
+    const GROUNDHOGS_ALL = 89
+    const GROUNDHOGS_CANADA = 15
     const GROUNDHOGS_USA = 74
-    const GROUNDHOGS_ALTERNATIVE = 51
-    const GROUNDHOGS_ACTIVE = 77
+    const GROUNDHOGS_ALTERNATIVE = 52
+    const GROUNDHOGS_ACTIVE = 78
 
     const urls = [
       {
